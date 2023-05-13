0ca203663de1dc70792fe453ea4b90d6c41b10ce
Pongthorn Chumpoo <chumpoo.dev@gmail.com>
2023-05-13 22:13:40 +0700

    added questions 18 and 19

---
+++ Chapter-1-Python-Primer/R1-19.py
@@ -0,0 +1,4 @@
+# Demonstrate how to use Python’s list comprehension syntax to produce thelist[ a , b , c ,..., z ],
+# but without having to typeall 26such characters literally.
+
+print([chr(i) for i in range(97,123)])
